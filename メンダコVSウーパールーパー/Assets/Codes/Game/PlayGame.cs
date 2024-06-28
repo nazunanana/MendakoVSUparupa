6eb87f1ec941c70c8547c81fe0129a87073413cd
uyuuuuu <soufflesumire.03@gmail.com>
2024-06-28 21:20:27 +0900

    Select Grid
    SC_Gameのオブジェクト生成システムを改修、2playerを生成するように
    コマのカーソル位置に合わせた強調、クリックによる状態変化

---
+++ メンダコVSウーパールーパー/Assets/Codes/Game/PlayGame.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{
+    private GameObject PL_uparupa;
+    private GameObject PL_mendako;
+    public GameObject playerPrehab;
+    private GameObject manageGrid;
+    public GameObject gridSystemPrehab;
+    void Start()
+    {
+        // プレイヤーオブジェクトを作成
+        PL_uparupa = Instantiate(playerPrehab, playerPrehab.transform.position, Quaternion.identity);
+        PL_mendako = Instantiate(playerPrehab, playerPrehab.transform.position, Quaternion.identity);
+        PL_uparupa.GetComponent<PlayerState>().setgetTeam = PlayerState.Team.uparupa;
+        PL_mendako.GetComponent<PlayerState>().setgetTeam = PlayerState.Team.mendako;
+        // 管理オブジェクト生成
+        manageGrid = Instantiate(gridSystemPrehab, gridSystemPrehab.transform.position, Quaternion.identity);
+        manageGrid.GetComponent<ManageGrid>().SetPlayers = new GameObject[]{PL_uparupa, PL_mendako};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

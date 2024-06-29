cd99b12d890873ba7df63a34880f6f4848b340de
uyuuuuu <soufflesumire.03@gmail.com>
2024-06-29 11:00:33 +0900

    Select system
    PlayerStateの改修、ゲームのメインクラスPlayGameを追加
    マスをカーソル位置でハイライトするシステムを実装

--- メンダコVSウーパールーパー/Assets/Codes/Game/PlayerState.cs
+++ メンダコVSウーパールーパー/Assets/Codes/Game/PlayerState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 /// <summary>
 /// プレイヤーの状態
@@ -23,10 +24,32 @@ public class PlayerState : MonoBehaviour
     // メンダコ陣営のカメラ位置、向き（定数）
     private const int MEN_CAMERA_POSITION_Z = 13;
     private const int MEN_CAMERA_ROTATION_Y = 180;
+    private bool isMyTurn;
+    // 駒を選択中
+    private bool isSelectingPiece;
+    // 移動マスを選択中
+    private bool isSelectingMove;
+    // 選択した駒位置
+    private Vector2Int piecePos;
+    // 選択した移動先
+    private Vector2Int moveToPos;
     
+    public void Init(Team team){
+        if(team==Team.uparupa){
+            this.isMyTurn = true;
+            this.isSelectingPiece = true;
+        }else{
+            this.isMyTurn = false;
+            this.isSelectingPiece = false;
+        }
+        this.isSelectingMove = false;
+    }
     public Team setgetTeam{
         get{ return team; }
-        set{ team = value; }
+        set{
+            team = value;
+            Init(team);
+        }
     }
 
     public int getsetRealPoint{
@@ -37,5 +60,41 @@ public class PlayerState : MonoBehaviour
     public int getsetFakePoint{
         get{ return fakePoint; }
         set{ fakePoint = value; }
+    }
+
+    public Vector2Int getPiecePosition{
+        get{ return piecePos; }
+    }
+
+    public Vector2Int getMoveToPos{
+        get{ return moveToPos; }
+    }
+
+    public bool getisSelectingPiece{
+        get{ return isSelectingPiece; }
+    }
+    public bool getisSelectingMove{
+        get{ return isSelectingMove; }
+    }
+
+
+    // ターン開始
+    public void StartMyTurn(){
+        isSelectingPiece = true;
+        isSelectingMove = false;
+    }
+    // 駒選択した
+    public void SelectPiece(Vector2Int posID){
+        piecePos = posID;
+        isSelectingPiece = false;
+        isSelectingMove = true;
+        Debug.Log("select piece on "+piecePos[0]+","+piecePos[1]);
+    }
+    // 移動マス選択したターンエンド
+    public void MovePiece(Vector2Int posID){
+        moveToPos = posID;
+        Debug.Log("move to "+moveToPos[0]+","+moveToPos[1]);
+        isSelectingPiece = false;
+        isSelectingMove = false;
     }
 }

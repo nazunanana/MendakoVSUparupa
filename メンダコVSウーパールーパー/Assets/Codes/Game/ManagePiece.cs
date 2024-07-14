25ca14a2e5271dec08969c12efd4f7e5133c047a
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-14 14:10:52 +0900

    Create ManagePiece.cs

---
+++ メンダコVSウーパールーパー/Assets/Codes/Game/ManagePiece.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagePiece : MonoBehaviour
+{
+    private GameObject player;
+    private int getPieceNum;
+
+    private Dictionary
+
+}

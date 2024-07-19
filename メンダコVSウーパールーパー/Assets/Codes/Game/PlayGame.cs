bee698ba7c83e412ba2eee279759f487a773669e
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-20 01:06:27 +0900

    cant move
    動けない駒を選択できないように

--- メンダコVSウーパールーパー/Assets/Codes/Game/PlayGame.cs
+++ メンダコVSウーパールーパー/Assets/Codes/Game/PlayGame.cs
@@ -168,29 +168,6 @@ public class PlayGame : NetworkBehaviour
     }
 
     /// <summary>
-    /// 前後左右の駒を検索
-    /// </summary>
-    public int[] SearchWASD(Vector2Int posID)
-    {
-        int id_x = posID.x;
-        int id_z = posID.y;
-        int w = -1,
-            a = -1,
-            s = -1,
-            d = -1;
-
-        if (0 <= id_x - 1)
-            w = SearchPieceByPos(new Vector2Int(id_x - 1, id_z)); // 上のマス
-        if (0 <= id_z - 1)
-            a = SearchPieceByPos(new Vector2Int(id_x, id_z - 1)); // 左のマス
-        if (id_x + 1 < GRID_NUM)
-            s = SearchPieceByPos(new Vector2Int(id_x + 1, id_z)); // 下のマス
-        if (id_z + 1 < GRID_NUM)
-            d = SearchPieceByPos(new Vector2Int(id_x, id_z + 1)); // 右のマス
-        return new int[] { w, a, s, d }; //上左下右 -1:範囲外 0:null 1:自陣の駒 2:相手の駒
-    }
-
-    /// <summary>
     /// 指定位置の駒を検索 自分の駒:1 相手の駒:2 なし:0
     /// </summary>
     public int SearchPieceByPos(Vector2Int posID)
@@ -200,7 +177,7 @@ public class PlayGame : NetworkBehaviour
 
         if (myplayer.GetComponent<ManagePiece>().pieceDic.ContainsKey(posID))
             return 1;
-        else if (partnerplayer.GetComponent<ManagePiece>().syncDic.ContainsKey(posID))
+        else if (partnerplayer.GetComponent<ManagePiece>().syncDic.ContainsKey(posID)) //TODO:0になる
             return 2;
         else
             return 0;
